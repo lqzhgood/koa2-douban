3389958179f46fa70064d6a213641e738413a15c
lqzhgood <lqzhgood@users.noreply.github.com>
2018-09-11 17:09:17 +0800

    5-2

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const app = new Koa();
+const { normal } = require('./tpl/index.js')
 
 app.use(async (ctx, next) => {
-	ctx.body = 'hello world'
+	ctx.type = "text/html; charset=utf-8"
+	ctx.body = normal
 })
 
 app.listen(3000)
