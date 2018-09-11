a51e8337574b23c89c7ac299a16ab560062d6ab6
lqzhgood <lqzhgood@users.noreply.github.com>
2018-09-11 18:10:27 +0800

    5-3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa')
 const app = new Koa();
-const { normal } = require('./tpl/index.js')
+const ejs = require('ejs')
+
+const { htmlTpl, ejsTpl } = require('./tpl/index.js')
 
 app.use(async (ctx, next) => {
 	ctx.type = "text/html; charset=utf-8"
-	ctx.body = normal
+	ctx.body = ejs.render(ejsTpl, {
+		you: 'lqzh',
+		me: 'gaga'
+	})
 })
 
 app.listen(3000)
